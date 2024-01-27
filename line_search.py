5516939ba11428f3d7c3db4d61ce94faf78e6284
Joseph <eeroward@gmail.com>
2024-01-27 09:13:12 -0700

    Created the line search file.

---
+++ line_search.py
@@ -0,0 +1,23 @@
+"""
+Created on Jan 27th for Homework 2 and 3 for ME 575
+by Joseph Ward
+
+Line Search Algorithm
+"""
+
+def bracketing():
+    pass
+
+def pinpointing():
+    pass
+
+# For Homework 3
+# After minimizing sufficently along a direction, change the direction
+def change_direction():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
