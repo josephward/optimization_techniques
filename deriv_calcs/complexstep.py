cc2256da6986e8939a55964386e92af77b6b4dac
Joseph <eeroward@gmail.com>
2024-02-12 11:25:11 -0700

    Created a folder to hold derivative calculators. Initially this will be organized according to the homework, but eventually this will be organized by method.

---
+++ deriv_calcs/complexstep.py
@@ -0,0 +1,17 @@
+"""
+Solution for Derivatives using Complex Step
+
+Created by Joseph Ward on Feb 12th, 2024
+"""
+
+#Example Function
+
+#Complex Step function
+
+#Forward Finite Differencing
+
+def main():
+    pass
+
+if __name__ = "__main__":
+    main()
